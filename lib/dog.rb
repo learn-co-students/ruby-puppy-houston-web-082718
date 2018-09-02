226e7bd5fe4e3c7b64e625dc580b8519379ee6f4
Naeblis <johnmlowery@gmail.com>
2018-09-02 10:38:52 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+require "pry"
+
+class Dog
+
+
+    @@all = []
+
+    attr_accessor :name
+
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.all
+        @@all.collect do |dog|
+            puts dog.name
+        end
+        
+    end
+
+ 
+
+    # binding.pry
+end
